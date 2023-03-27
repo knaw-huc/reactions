d0153410299a7018afc754f009264466983d4f9f
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-03-27 15:13:02 +0200

    Basic full text search component

---
+++ src/lib/fullTextSearch.tsx
@@ -0,0 +1,44 @@
+//Code adapted from: https://github.com/knaw-huc/browser-base-react/blob/4e09cd3e911a4877a6d88402696f55114cff09e2/src/facets/freeTextFacet.tsx
+import React from "react";
+
+interface FullTextFacetProps {
+  valueHandler: (value: string) => void;
+}
+
+export default function FullTextFacet(props: FullTextFacetProps) {
+  const [textField, setTextField] = React.useState("");
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setTextField(event.currentTarget.value);
+  };
+
+  const keyPressHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      props.valueHandler(event.currentTarget.value);
+    }
+  };
+
+  const buttonClickHandler = () => {
+    props.valueHandler(textField);
+  };
+
+  return (
+    <div className="hcFacet">
+      <div className="hcFacetTitle">Text search</div>
+      <div className="hcFacetSearch">
+        <input
+          type="text"
+          name=""
+          id="freeText"
+          value={textField}
+          placeholder="Press ENTER to search"
+          onChange={handleChange}
+          onKeyUp={keyPressHandler}
+        />
+        <button type="button" name="button" onClick={buttonClickHandler}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 import Button from "./Button";
+import FullTextFacet from "./fullTextSearch";
 
-export { Button };
+export { Button, FullTextFacet };
